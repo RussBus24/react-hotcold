c9286e8d851bcaa47babd4289ac0da6bf2425e2d
Russell Henry <rjhenry1014@gmail.com>
2017-01-20 22:14:53 +0000

    Added Game and Board component

--- js/components/game.js
+++ js/components/game.js
@@ -2,11 +2,20 @@ var React = require('react');
 var connect = require('react-redux').connect;
 
 var actions = require('../actions/index');
+var Board = require('./board');
 
 var Game = React.createClass({
     
     render: function() {
         
+        return (
+        <div className="game">
+            <h1>HOT and COLD</h1>
+            <Board />
+        </div>
+        );
     }
     
 });
+
+module.exports = Game;
